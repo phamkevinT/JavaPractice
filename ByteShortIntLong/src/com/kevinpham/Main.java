ff473f06550bd77d747524d2e1356ff607ba4b4b
Kevin Pham <phambamkevin@gmail.com>
2020-10-31 22:47:25 -0700

    Added Max/Min values for Byte and Short

--- ByteShortIntLong/src/com/kevinpham/Main.java
+++ ByteShortIntLong/src/com/kevinpham/Main.java
@@ -9,6 +9,16 @@ public class Main {
         System.out.println("Integer Minimum Value: " + myMinIntValue);
         System.out.println("Integer Maximum Value: " + myMaxIntValue);
 
-        // This is a test
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        byte myMinByteValue = Byte.MIN_VALUE;
+        System.out.println("Byte Minimum Value: " + myMinByteValue);
+        System.out.println("Byte Maximum Value: " + myMaxByteValue);
+
+        short myMaxShortValue = Short.MAX_VALUE;
+        short myMinShortValue = Short.MIN_VALUE;
+        System.out.println("Short Minimum Value: " + myMinShortValue);
+        System.out.println("Short Maximum Value: " + myMaxShortValue);
+
+
     }
 }
