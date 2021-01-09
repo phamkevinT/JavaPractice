440b8a2cfa4c07f6dda3bdb5be8b8527e523a9f5
Kevin Pham <phambamkevin@gmail.com>
2021-01-09 15:31:32 -0800

    Created project for basic object-oriented programming challenge

---
+++ OOP_Challenge_Basic/src/com/kevinpham/Main.java
@@ -0,0 +1,23 @@
+package com.kevinpham;
+
+//  The purpose of this application is to help a company called Bill's Burgers manage the process of selling their hamburger.
+//
+//  For the base Hamburger class, there are four variables to represent the four basic ingredients of the hamburger, (name, meat, price, breadRollType)
+//
+//  There are 4 seperate variables that are additional add-ons to the hamburger.
+//
+//  Four methods are needed for adding the 4 different add-ons. Each method takes in a 'name' and a 'price'.
+//  Another method calculates the hamburger's price including any add-ons.
+//
+//
+//  A second class DeluxeBurger, without any member variables, is needed for another type of burger. This class has a constructor
+//  without any parameters. The DeluxeBurger comes with chips and a drink. However, no additional add-ons can be added.class
+//
+//  A third class HealthyBurger allows for 2 additional healthy add-ons.
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
