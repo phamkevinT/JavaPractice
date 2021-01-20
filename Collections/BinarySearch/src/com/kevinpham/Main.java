596e642219fe0cfaa8fed70213d24b6a34d8639d
Kevin Pham <phambamkevin@gmail.com>
2021-01-19 18:18:32 -0800

    Binary Search Example

--- Collections/BinarySearch/src/com/kevinpham/Main.java
+++ Collections/BinarySearch/src/com/kevinpham/Main.java
@@ -3,6 +3,16 @@ package com.kevinpham;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Theatre theatre = new Theatre("Olympian", 8, 12);
+
+//        theatre.getSeats();
+
+        if (theatre.reserveSeat("B10")) {
+            System.out.println("Please pay");
+        } else {
+            System.out.println("Sorry, seat is taken");
+        }
+
     }
 }
+
