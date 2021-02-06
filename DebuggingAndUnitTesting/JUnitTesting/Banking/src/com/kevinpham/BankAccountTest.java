3cb0affbd1737eecf2f9ebf364d61321149bdd37
Kevin Pham <phambamkevin@gmail.com>
2021-02-06 15:11:27 -0800

    Set up test calss

---
+++ DebuggingAndUnitTesting/JUnitTesting/Banking/src/com/kevinpham/BankAccountTest.java
@@ -0,0 +1,18 @@
+package com.kevinpham;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BankAccountTest {
+
+    @org.junit.jupiter.api.Test
+    void deposit() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void withdraw() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getBalance() {
+    }
+}
