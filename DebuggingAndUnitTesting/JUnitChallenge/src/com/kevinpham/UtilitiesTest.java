8f9584e9882d3cf0c53705136ad4d141c2f0fac2
Kevin Pham <phambamkevin@gmail.com>
2021-02-08 13:20:54 -0800

    Created Test Class by ALT+ENTER after Utilities class name

---
+++ DebuggingAndUnitTesting/JUnitChallenge/src/com/kevinpham/UtilitiesTest.java
@@ -0,0 +1,26 @@
+package com.kevinpham;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UtilitiesTest {
+
+    @org.junit.jupiter.api.Test
+    void everyNthChar() {
+        fail("This test has not been implemented.");
+    }
+
+    @org.junit.jupiter.api.Test
+    void removePairs() {
+        fail("This test has not been implemented.");
+    }
+
+    @org.junit.jupiter.api.Test
+    void converter() {
+        fail("This test has not been implemented.");
+    }
+
+    @org.junit.jupiter.api.Test
+    void nullIfOddLength() {
+        fail("This test has not been implemented.");
+    }
+}
