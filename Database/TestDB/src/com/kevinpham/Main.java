3ed90aec521b7deb72caceca904930b955080be8
Kevin Pham <phambamkevin@gmail.com>
2021-02-08 17:10:45 -0800

    Added sqlite JDBC driver jar file

--- Database/TestDB/src/com/kevinpham/Main.java
+++ Database/TestDB/src/com/kevinpham/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            Connection conn = DriverManager.getConnection("jdbc:sqlite:D:\\github\\JavaMasterClass\\Database\\testjava.db");
+            Connection conn = DriverManager.getConnection("jdbc:sqlite:D:\\github\\JavaMasterClass\\Database\\TestDB\\testjava.db");
         } catch (SQLException e) {
             System.out.println("Something went wrong: " + e.getMessage());
         }
