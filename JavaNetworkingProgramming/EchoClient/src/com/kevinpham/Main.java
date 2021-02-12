93ebc0857ac52e1a5ae7f35c1741d8929c654c9b
Kevin Pham <phambamkevin@gmail.com>
2021-02-12 15:00:08 -0800

    Code to create a new thread everytime connect toc client

--- JavaNetworkingProgramming/EchoClient/src/com/kevinpham/Main.java
+++ JavaNetworkingProgramming/EchoClient/src/com/kevinpham/Main.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
--- JavaNetworkingProgramming/EchoServer/src/com/kevinpham/Main.java
+++ JavaNetworkingProgramming/EchoServer/src/com/kevinpham/Main.java
@@ -1,9 +1,6 @@
 package com.kevinpham;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -14,22 +11,13 @@ public class Main {
         // Create a server socket
         try (ServerSocket serverSocket = new ServerSocket(5000)) {
 
-            // Socket instance that is used to communicate with the client
-            Socket socket = serverSocket.accept();
-            System.out.println("Client Connected");
-
-            // IO Stream to send and receive data from client
-            BufferedReader input = new BufferedReader(
-                    new InputStreamReader(socket.getInputStream()));
-            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
-
-            // Read data from the client and print message
             while (true) {
-                String echoString = input.readLine();
-                if (echoString.equals("exit")) {
-                    break;
-                }
-                output.println("Echo from server: " + echoString);
+
+                // Kicking off new thread everytime we accept
+                Socket socket = serverSocket.accept();
+                Echoer echoer = new Echoer(socket);
+                echoer.start();
+
             }
 
 
