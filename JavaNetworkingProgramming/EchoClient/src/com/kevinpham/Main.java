7324c81f774e41a77aa6d00723b450103bce3304
Kevin Pham <phambamkevin@gmail.com>
2021-02-12 13:38:09 -0800

    Client to send data to Server

--- JavaNetworkingProgramming/EchoClient/src/com/kevinpham/Main.java
+++ JavaNetworkingProgramming/EchoClient/src/com/kevinpham/Main.java
@@ -1,8 +1,46 @@
 package com.kevinpham;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Create a Socket
+        try (Socket socket = new Socket("localhost", 5000)) {
+            BufferedReader echoes = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream()));
+            PrintWriter stringToEcho = new PrintWriter(socket.getOutputStream(), true);
+
+            // Take console input string to send to server
+            Scanner scanner = new Scanner(System.in);
+            String echoString;
+            String response;
+
+            do {
+                System.out.println("Enter string to be echoed: ");
+                echoString = scanner.nextLine();
+
+                // Write to server
+                stringToEcho.println(echoString);
+                if (!echoString.equals("exit")) {
+                    // Get response from server
+                    response = echoes.readLine();
+                    // Print response from server
+                    System.out.println(response);
+                }
+            } while (!echoString.equals("exit"));
+
+        } catch (IOException e) {
+            System.out.println("Client Error: " + e.getMessage());
+        }
+
     }
 }
