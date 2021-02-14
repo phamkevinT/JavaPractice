5bb4d9344e44557e8b1423c626cd02c17ad3da61
Kevin Pham <phambamkevin@gmail.com>
2021-02-14 12:36:20 -0800

    HttpURLConnection Example

--- JavaNetworkingProgramming/Networking/src/com/kevinpham/Main.java
+++ JavaNetworkingProgramming/Networking/src/com/kevinpham/Main.java
@@ -3,10 +3,10 @@ package com.kevinpham;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLConnection;
+import java.net.*;
+import java.sql.SQLOutput;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
 
@@ -14,19 +14,57 @@ public class Main {
 
         try {
             URL url = new URL("http://example.org");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("User-Agent", "Chrome");
+            // 30 Second timeout
+            connection.setReadTimeout(3000);
 
-            URLConnection urlConnection = url.openConnection();
-            urlConnection.connect();
+            // Setting response code, also performs the connection
+            int responseCode = connection.getResponseCode();
+            System.out.println("Response code: " + responseCode);
 
-            BufferedReader inputStream = new BufferedReader(
-                    new InputStreamReader(urlConnection.getInputStream()));
+            // Response code 200 = OK
+            if (responseCode != 200) {
+                System.out.println("Error reading web page");
+                return;
+            }
+
+            BufferedReader inputReader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
 
             String line = "";
-            while (line != null) {
-                line = inputStream.readLine();
+            // Read content of website
+            while ((line = inputReader.readLine()) != null) {
                 System.out.println(line);
             }
-            inputStream.close();
+            inputReader.close();
+
+//
+//            // *** Alternative ***
+//
+//            urlConnection.connect();
+//
+//            BufferedReader inputStream = new BufferedReader(
+//                    new InputStreamReader(urlConnection.getInputStream()));
+//
+//            Map<String, List<String>> headerFields = connection.getHeaderFields();
+//            for (Map.Entry<String, List<String>> entry : headerFields.entrySet()) {
+//                String key = entry.getKey();
+//                List<String> value = entry.getValue();
+//                System.out.println("--------key = " + key);
+//                for (String string : value) {
+//                    System.out.println("value = " + value);
+//                }
+//            }
+//
+//
+//            String line = "";
+//            while (line != null) {
+//                line = inputStream.readLine();
+//                System.out.println(line);
+//            }
+//            inputStream.close();
 
 
         } catch (MalformedURLException e) {
